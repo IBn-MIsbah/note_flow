70dd2076de8a06eb0035e3bab3b94a4d3de2ca14
ibn-misbah <kawnuma@gmail.com>
2026-01-18 20:39:30 +0300

    initial commit

---
+++ web/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+
+export const metadata: Metadata = {
+  title: "Note Flow",
+  description: "",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
