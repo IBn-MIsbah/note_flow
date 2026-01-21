cf118ce688b0502656c131797496465c0722a684
ibn-misbah <kawnuma@gmail.com>
2026-01-21 16:03:04 +0300

    feat: regster users route

--- server/app/api/v1/api.py
+++ server/app/api/v1/api.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 
-from app.api.v1 import auth
+from app.api.v1 import auth, user
 
 api_router = APIRouter(prefix="/api/v1")
 
 api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
+api_router.include_router(user.router, prefix="/users", tags=["users"])
