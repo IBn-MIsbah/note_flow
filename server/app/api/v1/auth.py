3396b0c6ff1908b8e39a9728cacbc8c16639a887
ibn-misbah <kawnuma@gmail.com>
2026-01-21 18:05:15 +0300

    feat: GET /me route to fetch curret user data
    POST /refresh-token to rotate jwt

--- server/app/api/v1/auth.py
+++ server/app/api/v1/auth.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException, Response, status
+from fastapi import APIRouter, Depends, HTTPException, Request, Response, status
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -6,9 +6,10 @@ from app.core.db import get_db
 from app.core.security import create_access_token as cat
 from app.core.security import create_refresh_token as crt
 from app.core.security import set_auth_cookeis as sac
-from app.core.security import verify_password
+from app.core.security import verify_password, decode_refresh_token as drt
 from app.models import User
-from app.schema import LoginInput
+from app.schema import LoginInput, UserResponse
+from app.api.deps import get_current_user
 
 router = APIRouter()
 
@@ -43,3 +44,30 @@ async def login(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail=f"Login error: {str(e)}",
         )
+
+@router.get('/me', status_code=200, response_model=UserResponse)
+async def read_me(current_user:User = Depends(get_current_user)):
+    return current_user
+
+@router.post('/refresh-token', status_code=204)
+async def refresh_token(request:Request, response:Response, session:AsyncSession = Depends(get_db)):
+    refresh_token = request.cookies.get('refresh_token')
+    if not refresh_token:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Session Expired. Please Login to continue."
+        )
+    
+    user_id=await drt(refresh_token=refresh_token)
+    user = await session.get(User, user_id)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found"
+        )
+    
+    new_access_token = cat(user.id)
+    new_refresh_token = crt(user.id)
+    sac(access_token=new_access_token, refresh_token=new_refresh_token, response=response)
+
+    return None
