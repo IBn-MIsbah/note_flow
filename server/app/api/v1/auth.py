c6070e620621f1424a42a88a1de3a071c34be9fd
ibn-misbah <kawnuma@gmail.com>
2026-01-21 18:06:18 +0300

    formated

--- server/app/api/v1/auth.py
+++ server/app/api/v1/auth.py
@@ -1,15 +1,17 @@
-from fastapi import APIRouter, Depends, HTTPException, Request, Response, status
+from fastapi import (APIRouter, Depends, HTTPException, Request, Response,
+                     status)
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.api.deps import get_current_user
 from app.core.db import get_db
 from app.core.security import create_access_token as cat
 from app.core.security import create_refresh_token as crt
+from app.core.security import decode_refresh_token as drt
 from app.core.security import set_auth_cookeis as sac
-from app.core.security import verify_password, decode_refresh_token as drt
+from app.core.security import verify_password
 from app.models import User
 from app.schema import LoginInput, UserResponse
-from app.api.deps import get_current_user
 
 router = APIRouter()
 
@@ -45,29 +47,36 @@ async def login(
             detail=f"Login error: {str(e)}",
         )
 
-@router.get('/me', status_code=200, response_model=UserResponse)
-async def read_me(current_user:User = Depends(get_current_user)):
+
+@router.get("/me", status_code=200, response_model=UserResponse)
+async def read_me(current_user: User = Depends(get_current_user)):
     return current_user
 
-@router.post('/refresh-token', status_code=204)
-async def refresh_token(request:Request, response:Response, session:AsyncSession = Depends(get_db)):
-    refresh_token = request.cookies.get('refresh_token')
+
+@router.post("/refresh-token", status_code=204)
+async def refresh_token(
+    request: Request, response: Response, session: AsyncSession = Depends(get_db)
+):
+    refresh_token = request.cookies.get("refresh_token")
     if not refresh_token:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Session Expired. Please Login to continue."
+            detail="Session Expired. Please Login to continue.",
         )
-    
-    user_id=await drt(refresh_token=refresh_token)
+
+    user_id = await drt(refresh_token=refresh_token)
     user = await session.get(User, user_id)
     if not user:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="User not found"
+            status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
         )
-    
+
     new_access_token = cat(user.id)
     new_refresh_token = crt(user.id)
-    sac(access_token=new_access_token, refresh_token=new_refresh_token, response=response)
+    sac(
+        access_token=new_access_token,
+        refresh_token=new_refresh_token,
+        response=response,
+    )
 
     return None
