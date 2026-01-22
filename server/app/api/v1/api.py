fe11418f470da257c3642bd771311aab423918b9
ibn-misbah <kawnuma@gmail.com>
2026-01-22 22:15:11 +0300

    feat: register notes route

--- server/app/api/v1/api.py
+++ server/app/api/v1/api.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 
-from app.api.v1 import auth, user
+from app.api.v1 import auth, note, user
 
 api_router = APIRouter(prefix="/api/v1")
 
 api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
 api_router.include_router(user.router, prefix="/users", tags=["users"])
+api_router.include_router(note.router, prefix="/notes", tags=["notes"])
