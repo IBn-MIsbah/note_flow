96f593751e87c91d35fad5e7c7bb9ec52e96bd31
ibn-misbah <kawnuma@gmail.com>
2026-01-23 21:38:28 +0300

    feat: register note lock api

--- server/app/api/v1/api.py
+++ server/app/api/v1/api.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
 
-from app.api.v1 import auth, note, user
+from app.api.v1 import auth, note, user, note_lock
 
 api_router = APIRouter(prefix="/api/v1")
 
 api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
 api_router.include_router(user.router, prefix="/users", tags=["users"])
 api_router.include_router(note.router, prefix="/notes", tags=["notes"])
+api_router.include_router(note_lock.router, prefix="/locks", tags=["note_locks"])
